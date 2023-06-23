7e632a04c338b43ba06987c75f9677888dfd07c4
kenji <duyhung71089@gmail.com>
2023-06-23 12:06:46 +0700

    chore: remove apikey from code

--- example/lib/constant.dart
+++ example/lib/constant.dart
@@ -1,8 +1,8 @@
 abstract class Constants {
   // Configuring apps with compilation environment declarations
   // https://dart.dev/guides/environment-declarations
-  static const API_KEY = String.fromEnvironment("API_KEY", defaultValue: "68f36f74aed68a63c6a0de5cf2de2f343c3714c0d1be4083f8fa679a39644a7c");
-  static const API_ENDPOINT = String.fromEnvironment("API_ENDPOINT", defaultValue:"https://api-dev.bucketeer.jp");
+  static const API_KEY = String.fromEnvironment("API_KEY", defaultValue: "*****************************");
+  static const API_ENDPOINT = String.fromEnvironment("API_ENDPOINT", defaultValue:"https://************.bucketeer.jp");
   static const DEFAULT_FEATURE_TAG = "ios";
   static const DEFAULT_USERID = "bucketeer-ios-user-id-1";
   static const DEFAULT_EVENTS_FLUSH_INTERVAL =  60000;
