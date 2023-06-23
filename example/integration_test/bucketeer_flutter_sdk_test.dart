d6a94e72a780f00e189c0608719433184e733739
kenji <duyhung71089@gmail.com>
2023-06-23 21:37:11 +0700

    feat: E2E test now using flutter test data

--- example/integration_test/bucketeer_flutter_sdk_test.dart
+++ example/integration_test/bucketeer_flutter_sdk_test.dart
@@ -12,27 +12,27 @@ void main() async {
   const String OLD_APP_VERSION = "0.0.1";
   const bool DEBUGGING = true;
 
-  // E2E iOS
-  const String FEATURE_TAG = "ios";
-  const String USER_ID = 'bucketeer-ios-user-id-1';
+  // E2E Flutter
+  const String FEATURE_TAG = "flutter";
+  const String USER_ID = 'bucketeer-flutter-user-id-1';
 
-  const String FEATURE_ID_BOOLEAN = "feature-ios-e2e-bool";
+  const String FEATURE_ID_BOOLEAN = "feature-flutter-e2e-boolean";
   const bool FEATURE_ID_BOOLEAN_VALUE = true;
 
-  const String FEATURE_ID_STRING = "feature-ios-e2e-string";
+  const String FEATURE_ID_STRING = "feature-flutter-e2e-string";
   const String FEATURE_ID_STRING_VALUE = "value-1";
   const String FEATURE_ID_STRING_VALUE_UPDATE = "value-2";
 
-  const String FEATURE_ID_INT = "feature-ios-e2e-integer";
+  const String FEATURE_ID_INT = "feature-flutter-e2e-int";
   const int FEATURE_ID_INT_VALUE = 10;
 
-  const String FEATURE_ID_DOUBLE = "feature-ios-e2e-double";
+  const String FEATURE_ID_DOUBLE = "feature-flutter-e2e-double";
   const double FEATURE_ID_DOUBLE_VALUE = 2.1;
 
-  const String FEATURE_ID_JSON = "feature-ios-e2e-json";
+  const String FEATURE_ID_JSON = "feature-flutter-e2e-json";
   const Map<String, dynamic> FEATURE_ID_JSON_VALUE = {"key": "value-1"};
 
-  const String GOAL_ID = "goal-ios-e2e-1";
+  const String GOAL_ID = "goal-flutter-e2e-1";
   const double GOAL_VALUE = 1.0;
 
   void runAllTests() {
@@ -51,11 +51,11 @@ void main() async {
           await Bucketeer.instance.evaluationDetails(FEATURE_ID_STRING);
       var expected = const BKTResult.success(
           data: BKTEvaluation(
-              id: "feature-ios-e2e-string:2:bucketeer-ios-user-id-1",
+              id: "$FEATURE_ID_STRING:3:$USER_ID",
               featureId: FEATURE_ID_STRING,
-              featureVersion: 2,
+              featureVersion: 3,
               userId: USER_ID,
-              variationId: "349ed945-d2f9-4d04-8e83-82344cffd1ec",
+              variationId: "2e696c59-ac2f-4b54-82a7-4aecfdd80224",
               variationValue: "value-1",
               reason: "DEFAULT"));
       expect(result, expected);
@@ -75,11 +75,11 @@ void main() async {
           await Bucketeer.instance.evaluationDetails(FEATURE_ID_DOUBLE);
       var expected = const BKTResult.success(
           data: BKTEvaluation(
-              id: "feature-ios-e2e-double:2:bucketeer-ios-user-id-1",
+              id: "$FEATURE_ID_DOUBLE:2:$USER_ID",
               featureId: FEATURE_ID_DOUBLE,
               featureVersion: 2,
               userId: USER_ID,
-              variationId: "38078d8f-c6eb-4b93-9d58-c3e57010983f",
+              variationId: "a141d1fa-85ef-4124-af5e-25374225474b",
               variationValue: "2.1",
               reason: "DEFAULT"));
       expect(result, expected);
@@ -99,11 +99,11 @@ void main() async {
           await Bucketeer.instance.evaluationDetails(FEATURE_ID_BOOLEAN);
       var expected = const BKTResult.success(
           data: BKTEvaluation(
-              id: "feature-ios-e2e-bool:2:bucketeer-ios-user-id-1",
+              id: "$FEATURE_ID_BOOLEAN:2:$USER_ID",
               featureId: FEATURE_ID_BOOLEAN,
               featureVersion: 2,
               userId: USER_ID,
-              variationId: "4f9e0f88-e053-42a9-93e1-95d407f67021",
+              variationId: "cbd42331-094e-4306-aacd-d7bf3f07cf65",
               variationValue: "true",
               reason: "DEFAULT"));
       expect(result, expected);
@@ -121,11 +121,11 @@ void main() async {
       var result = await Bucketeer.instance.evaluationDetails(FEATURE_ID_INT);
       var expected = const BKTResult.success(
           data: BKTEvaluation(
-              id: "feature-ios-e2e-integer:2:bucketeer-ios-user-id-1",
+              id: "$FEATURE_ID_INT:2:$USER_ID",
               featureId: FEATURE_ID_INT,
               featureVersion: 2,
               userId: USER_ID,
-              variationId: "9c5fd2d2-d587-4ba2-8de2-0fc9454d564e",
+              variationId: "36f14c02-300a-48f3-b4eb-b296afba3953",
               variationValue: "10",
               reason: "DEFAULT"));
       expect(result, expected);
@@ -141,11 +141,11 @@ void main() async {
       var result = await Bucketeer.instance.evaluationDetails(FEATURE_ID_JSON);
       var expected = const BKTResult.success(
           data: BKTEvaluation(
-              id: "feature-ios-e2e-json:2:bucketeer-ios-user-id-1",
+              id: "$FEATURE_ID_JSON:2:$USER_ID",
               featureId: FEATURE_ID_JSON,
               featureVersion: 2,
               userId: USER_ID,
-              variationId: "06f5be6b-0c79-431f-a057-822babd9d3eb",
+              variationId: "813070cf-7d6b-45a9-8713-cf9816d63997",
               variationValue: "{ \"key\": \"value-1\" }",
               reason: "DEFAULT"));
       expect(result, expected);
