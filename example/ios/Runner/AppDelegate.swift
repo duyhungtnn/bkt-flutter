01c173e5b443eeb24156984c902fe314ec1f1659
duyhungtnn <duyhung71089@gmail.com>
2023-10-20 14:30:35 +0700

    fix: update the iOS background mode settings for the demo app  (#7)
    * fix: update the background mode settings for the demo app
    
    * chore: update exmaple code to enable background mode for iOS

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+// 1.Import the SDK
+import Bucketeer
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // 2.Add the code to enable background tasks
+    if #available(iOS 13.0, tvOS 13.0, *) {
+        BKTBackgroundTask.enable()
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
