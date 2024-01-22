c94d4e92ecf174bd4d6534bfcffa2bd8c29b222e
duyhungtnn <duyhung71089@gmail.com>
2024-01-23 00:28:06 +0700

    fix: lint fail

--- lib/src/evaluation_update_listener_dispatcher.dart
+++ lib/src/evaluation_update_listener_dispatcher.dart
@@ -2,9 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'evaluation_update_listener.dart';
 
 class EvaluationUpdateListenerDispatcher {
-  // The native SDK listen token
-  static String? _proxyEvaluationUpdateListenToken;
-
   final _listeners = <String, BKTEvaluationUpdateListener>{};
 
   EvaluationUpdateListenerDispatcher(Stream<dynamic> eventStream) {
